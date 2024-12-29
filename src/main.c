62c70234b1c5e9b19fa5ab470979766c865b85b9
Maldus512 <mattia512maldini@gmail.com>
2024-12-29 16:38:56 +0100

    Custom log function

--- src/main.c
+++ src/main.c
@@ -2,13 +2,27 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+extern void    set_log_function(void (*log_function)(uint8_t *string));
+extern void    set_timestamp_function(uint64_t (*timestamp_function)(void));
+extern int16_t infer();
 
-extern int16_t infer(void);
+
+static void log_fn(uint8_t *string) {
+    printf("%s\n", string);
+}
+
+
+static uint64_t timestamp_fn(void) {
+    return to_us_since_boot(get_absolute_time());
+}
 
 
 int main() {
+    set_log_function(log_fn);
+    set_timestamp_function(timestamp_fn);
+
     setup_default_uart();
 
-    printf("Hello, world %i!\n", infer());
+    printf("Hello, world %i!\n", infer(log_fn));
     return 0;
 }
